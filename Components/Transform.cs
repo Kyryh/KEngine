c15d5e8009a68ef23c2a51250e90fda16fbf864f
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-08-08 20:30:48 +0200

    Added a way to get global attributes for transform

--- Components/Transform.cs
+++ Components/Transform.cs
@@ -9,5 +9,28 @@ namespace KEngine.Components
         public float rotation = 0f;
         public Vector2 scale = Vector2.One;
 
+        public Vector2 GlobalPosition {
+            get {
+                if (gameObject.parent == null)
+                    return position;
+                return gameObject.parent.Transform.GlobalPosition + position;
+            }
+        }
+
+        public float GlobalRotation {
+            get {
+                if (gameObject.parent == null)
+                    return rotation;
+                return gameObject.parent.Transform.GlobalRotation + rotation;
+            }
+        }
+
+        public Vector2 GlobalScale {
+            get {
+                if (gameObject.parent == null)
+                    return scale;
+                return gameObject.parent.Transform.GlobalScale + scale;
+            }
+        }
     }
 }
