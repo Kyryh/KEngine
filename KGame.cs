c81585cfacc6e8cecc37f5eb620a2b52d3b84594
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-08-08 21:51:41 +0200

    Create KGame.cs

---
+++ KGame.cs
@@ -0,0 +1,12 @@
+﻿using KEngine.Components;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEngine {
+    public abstract class KGame : Game {
+    }
+}
