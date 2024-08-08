8ba507a3a72caf3921ef567aae3712dfac68e13d
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-08-08 21:51:31 +0200

    Create DrawableComponent.cs

---
+++ Components/DrawableComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEngine.Components {
+    public abstract class DrawableComponent : Component {
+        public virtual int DrawingPriority => 0;
+    }
+}
