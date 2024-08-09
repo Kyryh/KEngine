c38619cd3d256959505b14cc3a3edc65a24fc714
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-08-09 12:48:42 +0200

    Create SpriteRenderer.cs

---
+++ Components/DrawableComponents/SpriteRenderer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEngine.Components.DrawableComponents {
+    public class SpriteRenderer : DrawableComponent {
+        public string spriteName;
+        float LayerDepth => 0f;
+        public override void Draw(SpriteBatch spriteBatch) {
+            var texture = KGame.GetContent<Texture2D>(spriteName);
+            spriteBatch.Draw(
+                texture,
+                Transform.GlobalPosition,
+                null,
+                Color.White,
+                Transform.GlobalRotation,
+                new Vector2(texture.Width/2f, texture.Height/2f),
+                Transform.GlobalScale,
+                SpriteEffects.None,
+                LayerDepth
+            );
+            
+            base.Draw(spriteBatch);
+
+        }
+    }
+}
