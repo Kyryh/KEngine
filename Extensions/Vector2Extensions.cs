6e388b6d17e44f7cc2863d2469c04daedaa8eb92
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-08-10 12:34:34 +0200

    Create Vector2Extensions.cs

---
+++ Extensions/Vector2Extensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEngine.Extensions {
+    public static class Vector2Extensions {
+        public static Vector3 ToVector3(this Vector2 v, float z = 0) {
+            return new Vector3(v.X, v.Y, z);
+        }
+    }
+}
