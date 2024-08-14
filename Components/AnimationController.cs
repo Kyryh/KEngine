5fe453a6bce490118cf6ddc7338ab4741f04c544
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-08-14 17:49:42 +0200

    Added animation support

---
+++ Components/AnimationController.cs
@@ -0,0 +1,68 @@
+﻿using KEngine.Components.DrawableComponents;
+using KEngine.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEngine.Components {
+    public class AnimationController : Component {
+        SpriteRenderer sr;
+        Dictionary<string, Animation> animations = new();
+        TimeSpan currentAnimationElapsedTime = TimeSpan.Zero;
+        public Animation CurrentAnimation { get; private set; }
+        int frameIndex = 0;
+        int FrameIndex {
+            get {
+                return frameIndex;
+            }
+            set {
+
+                frameIndex = value;
+
+                if (frameIndex >= CurrentAnimation.Frames) {
+                    SetAnimation(CurrentAnimation.NextAnimation, true);
+                } else {
+                    sr.spriteIndex = CurrentFrame.SpriteSheetIndex;
+                }
+            }
+        }
+        public Animation.Frame CurrentFrame => CurrentAnimation[FrameIndex];
+        public string StartingAnimation { get; init; }
+        public Animation[] Animations {
+            set {
+                foreach (var anim in value)
+                {
+                    animations[anim.Name] = anim;
+                }
+            }
+        }
+        public override void Initialize() {
+            base.Initialize();
+            sr = GameObject.GetComponent<SpriteRenderer>();
+
+            SetAnimation(StartingAnimation);
+            sr.spriteIndex = CurrentFrame.SpriteSheetIndex;
+        }
+
+        public void SetAnimation(string animationName, bool restartIfSameAnimation = false) {
+            if (!restartIfSameAnimation && animationName == CurrentAnimation?.Name)
+                return;
+            CurrentAnimation = animations[animationName];
+            currentAnimationElapsedTime = TimeSpan.Zero;
+            FrameIndex = 0;
+        }
+
+        public override void Update(float deltaTime) {
+            base.Update(deltaTime);
+            currentAnimationElapsedTime += TimeSpan.FromSeconds(deltaTime);
+            if (currentAnimationElapsedTime > CurrentFrame.Duration) {
+                currentAnimationElapsedTime -= CurrentFrame.Duration;
+                FrameIndex++;
+            }
+        }
+
+    }
+}
---
+++ Drawing/Animation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEngine.Drawing {
+    public class Animation {
+        public string Name { get; }
+        public string NextAnimation { get; }
+        public int Frames { get; }
+
+        private readonly Frame[] frames;
+        public Frame this[int index] => frames[index];
+        
+        public Animation(string name, string nextAnimation, params Frame[] frames) {
+            Name = name;
+            NextAnimation = nextAnimation ?? name;
+            this.frames = (Frame[])frames.Clone();
+            Frames = this.frames.Length;
+        }
+
+        public Animation(string name, params Frame[] frames) : this(name, null, frames) { }
+
+
+        public record Frame(int SpriteSheetIndex, TimeSpan Duration);
+    }
+}
